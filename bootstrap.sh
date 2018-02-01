e3b7f20a994088ac7273a9fd18a5d2d265efb172
nadyawilliams <nadya@sdsc.edu>
2018-02-01 14:02:09 -0800

    move source distro to google drive

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# @Copyright@
+# @Copyright@
+#
+# set google url for downloading sources
+export SURL="https://drive.google.com/open?id=1GhZuRdqWzE2mZTJ87UEL0tay-Yh9oCP8"
+
+# download sources
+. /opt/rocks/share/devel/src/roll/etc/bootstrap-functions.sh
+
