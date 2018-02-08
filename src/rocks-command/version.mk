8531beba2128405b181ce0908a3b56d4e600fd14
nadyawilliams <nadya@sdsc.edu>
2018-02-07 17:01:08 -0800

    add files for centos7

--- src/rocks-command/version.mk
+++ src/rocks-command/version.mk
@@ -1,2 +1,13 @@
 NAME    = rocks-command-gpupt
-RELEASE = 1
+RELEASE = 0
+
+RPM.FILES = \
+$(PY.ROCKS)/*egg-info \n\
+$(PY.ROCKS)/rocks/commands/report/host/vm/config/* \n\
+$(PY.ROCKS)/rocks/commands/report/host/gpu \n\
+$(PY.ROCKS)/rocks/commands/remove/host/gpu \n\
+$(PY.ROCKS)/rocks/commands/set/host/gpu \n\
+$(PY.ROCKS)/rocks/commands/dump/host/gpu \n\
+$(PY.ROCKS)/rocks/commands/add/host/gpu \n\
+$(PY.ROCKS)/rocks/commands/list/host/gpu \n\
+$(PKGROOT)/bin/*
